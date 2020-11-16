655eb00e20b3c0b576a603c4ff28a50ad4597350
MonicaHoward <jay.m.howard@gmail.com>
2020-11-16 15:21:57 -0600

    creates CustomerDaoImpl

---
+++ CyberBank5000/src/main/java/com/monicahoward/daoimpl/CustomerDaoImpl.java
@@ -0,0 +1,32 @@
+package com.monicahoward.daoimpl;
+
+import com.monicahoward.beans.Customer;
+import com.monicahoward.dao.CustomerDao;
+import com.monicahoward.utils.ConnFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+    public static ConnFactory cf = ConnFactory.getInstance();
+
+
+    @Override
+    public List<Customer> getAllCustomers() throws SQLException {
+        List<Customer>customerList= new ArrayList<Customer>();
+        Connection conn= cf.getConnection();
+        Statement stmt= conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from \"Customer\"");
+        Customer custo= null;
+        while(rs.next()){
+            custo= new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+            customerList.add(custo);
+        }
+        return customerList;
+    }
+}
